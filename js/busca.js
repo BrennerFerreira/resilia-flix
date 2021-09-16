754fd8085f1fc5b641e59ae1a58e1c6da35d377d
BrennerFerreira <brennercsferreira@gmail.com>
2021-09-16 11:05:20 -0300

    fix: corrigido url da api para utilizar https

--- js/busca.js
+++ js/busca.js
@@ -1,51 +1,49 @@
-$('#button-addon2').on('click', async function (event) {
-    $("#filmeBuscado").remove();
-    let textoBusca = $('#texto-busca').val();
-    buscarFilmeTitulo(textoBusca);
-    $('#footer').css({position:"relative"});
+$("#button-addon2").on("click", async function (event) {
+  $("#filmeBuscado").remove();
+  let textoBusca = $("#texto-busca").val();
+  buscarFilmeTitulo(textoBusca);
+  $("#footer").css({ position: "relative" });
 });
 
 function inserirFilmeNaPagina(filme) {
-    console.log(filme);
-    const divFilme = $(`
+  console.log(filme);
+  const divFilme = $(`
       <div class="text-center div-filme" id="filmeBuscado">
       <h2 class="titulo-filme">${filme.titulo}</h2>
       <img class="poster-filme" src=${filme.poster}>
       </div>
     `);
-  
-    $(divFilme).click(function () {
-      $("#modal-titulo").text(filme.titulo);
-      $("#modal-poster").attr("src", filme.poster);
-      $("#modal-duracao").html(`<b>Duração:</b> ${filme.duracao}</p>`);
-      $("#modal-genero").html(`<b>Gênero:</b> ${filme.genero}</p>`);
-      $("#modal-classificacao").html(
-        `<b>Classificação:</b> ${filme.classificacao}</p>`
-      );
-      $("#modal-plot").html(filme.plot);
-  
-      $("#filme-detalhes").modal("show");
-    });
-  
-    $("#filmes").append(divFilme);
-  }
+
+  $(divFilme).click(function () {
+    $("#modal-titulo").text(filme.titulo);
+    $("#modal-poster").attr("src", filme.poster);
+    $("#modal-duracao").html(`<b>Duração:</b> ${filme.duracao}</p>`);
+    $("#modal-genero").html(`<b>Gênero:</b> ${filme.genero}</p>`);
+    $("#modal-classificacao").html(
+      `<b>Classificação:</b> ${filme.classificacao}</p>`
+    );
+    $("#modal-plot").html(filme.plot);
+
+    $("#filme-detalhes").modal("show");
+  });
+
+  $("#filmes").append(divFilme);
+}
 
 function buscarFilmeTitulo(titulo) {
-    $.ajax({
-      url: `http://www.omdbapi.com/?apikey=da8a6c76&t=${titulo}`,
-      success: function (result) {
-        const novoFilme = new Filme(
-          result.Title,
-          result.Poster,
-          result.Rated,
-          result.Runtime,
-          result.Genre,
-          result.Plot
-        );
-  
-        inserirFilmeNaPagina(novoFilme);
-      },
-    });
-  }
-  
+  $.ajax({
+    url: `https://www.omdbapi.com/?apikey=da8a6c76&t=${titulo}`,
+    success: function (result) {
+      const novoFilme = new Filme(
+        result.Title,
+        result.Poster,
+        result.Rated,
+        result.Runtime,
+        result.Genre,
+        result.Plot
+      );
 
+      inserirFilmeNaPagina(novoFilme);
+    },
+  });
+}

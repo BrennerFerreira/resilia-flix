20aa5fed15ef9793856e08eaae86fde4e9f0a562
BrennerFerreira <brennercsferreira@gmail.com>
2021-09-17 08:50:00 -0300

    feat: login e logout extendido para toda a aplicacao

--- js/sessao.js
+++ js/sessao.js
@@ -9,3 +9,60 @@ function verificarSeUsuarioEstaLogado() {
 function fazerLogout() {
   window.sessionStorage.clear();
 }
+
+function alterarViewUsuarioDeslogado() {
+  $("#texto-nav").html(`
+    <a class="nav-link" href="./html/pagina-cadastro.html"
+      >Cadastre-se</a
+    >
+  `);
+
+  $("#texto-nav").removeClass(
+    "d-flex align-items-center justify-content-center px-1 logado"
+  );
+
+  const paginaAtual = window.location.pathname;
+
+  $("#botao-nav").html(`
+    <a
+      id="botao-login"
+      class="nav-link rounded text-light"
+      aria-current="page"
+      href=".${paginaAtual === "/index.html" ? "/html" : ""}/login.html"
+      >Entrar</a
+    >
+  `);
+}
+
+function botaoSairClicado() {
+  fazerLogout();
+  alterarViewUsuarioDeslogado();
+}
+
+function alterarViewUsuarioLogado(emailUsuario) {
+  $("#texto-nav").html(`
+    <div>${emailUsuario}</div>
+  `);
+
+  $("#texto-nav").addClass(
+    "d-flex align-items-center justify-content-center px-1 logado"
+  );
+
+  $("#botao-nav").html(`
+    <button class="btn btn-primary botao-sair">
+      Sair
+    </button>
+  `);
+
+  $(".botao-sair").click(botaoSairClicado);
+}
+
+function buscarUsuarioLogado() {
+  const emailUsuario = verificarSeUsuarioEstaLogado();
+
+  if (emailUsuario) {
+    alterarViewUsuarioLogado(emailUsuario);
+  }
+}
+
+$(buscarUsuarioLogado);

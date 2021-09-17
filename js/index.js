86ee0396aaad331a01aeb249a89d81e84aa82e9c
BrennerFerreira <brennercsferreira@gmail.com>
2021-09-16 22:14:51 -0300

    feat: deslogar usuario da sessao atual

--- js/index.js
+++ js/index.js
@@ -75,19 +75,56 @@ function buscarFilmesNaApi() {
   });
 }
 
+function alterarViewUsuarioDeslogado() {
+  $("#texto-nav").html(`
+    <a class="nav-link" href="./html/pagina-cadastro.html"
+      >Cadastre-se</a
+    >
+  `);
+
+  $("#texto-nav").removeClass(
+    "d-flex align-items-center justify-content-center px-1 logado"
+  );
+
+  $("#botao-nav").html(`
+    <a
+      id="botao-login"
+      class="nav-link rounded text-light"
+      aria-current="page"
+      href="./html/login.html"
+      >Entrar</a
+    >
+  `);
+}
+
+function botaoSairClicado() {
+  fazerLogout();
+  alterarViewUsuarioDeslogado();
+}
+
+function alterarViewUsuarioLogado(emailUsuario) {
+  $("#texto-nav").html(`
+    <div>${emailUsuario}</div>
+  `);
+
+  $("#texto-nav").addClass(
+    "d-flex align-items-center justify-content-center px-1 logado"
+  );
+
+  $("#botao-nav").html(`
+    <button class="btn btn-primary botao-sair">
+      Sair
+    </button>
+  `);
+
+  $(".botao-sair").click(botaoSairClicado);
+}
+
 function buscarUsuarioLogado() {
-  const emailUsuario = window.sessionStorage.getItem("email");
+  const emailUsuario = verificarSeUsuarioEstaLogado();
 
   if (emailUsuario) {
-    $("#texto-nav").html(`
-      <div>${emailUsuario}</div>
-    `);
-
-    $("#texto-nav").addClass(
-      "d-flex align-items-center justify-content-center px-1 logado"
-    );
-
-    $("#botao-login").text("Sair");
+    alterarViewUsuarioLogado(emailUsuario);
   }
 }
 
--- js/sessao.js
+++ js/sessao.js
@@ -2,6 +2,10 @@ function fazerLogin(email) {
   window.sessionStorage.setItem("email", email);
 }
 
+function verificarSeUsuarioEstaLogado() {
+  return window.sessionStorage.getItem("email");
+}
+
 function fazerLogout() {
   window.sessionStorage.clear();
 }
